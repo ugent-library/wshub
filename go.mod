625e2aad9ba8e297c176afa3f49cd91166a8e0d9
nsteenla <nicolas.steenlant@ugent.be>
2023-12-22 15:16:45 +0100

    support basic user presence

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ugent-library/hub
 go 1.21.5
 
 require (
+	github.com/google/uuid v1.5.0
 	golang.org/x/time v0.5.0
 	nhooyr.io/websocket v1.8.10
 )
--- hub.go
+++ hub.go
@@ -3,53 +3,82 @@ package hub
 import (
 	"context"
 	"errors"
-	"log"
 	"net"
 	"net/http"
 	"sync"
 	"time"
 
+	"github.com/google/uuid"
 	"golang.org/x/time/rate"
 	"nhooyr.io/websocket"
 )
 
 type Hub struct {
+	id            string
 	messageBuffer int
 	writeTimeout  time.Duration
 	limiter       *rate.Limiter
 	errorFunc     func(error)
-	topicFunc     func(*http.Request) []string
+	userFunc      func(*http.Request) (string, []string)
 	subscribersMu sync.RWMutex
 	subscribers   map[*subscriber]struct{}
 	topics        map[string]subscriptions
 }
 
+type subscriptions struct {
+	subscribers  map[string]Subscription
+	users        map[string]int
+	onNewUser    func(string, Subscription)
+	onRemoveUser func(string, Subscription)
+}
+
+type Subscription interface {
+	ID() string
+	UserID() string
+	Send([]byte)
+}
+
 type subscriber struct {
+	id        string
 	msgs      chan []byte
 	closeSlow func()
+	userID    string
 	topics    []string
 }
 
-type subscriptions struct {
-	subscribers map[*subscriber]struct{}
+func (s *subscriber) ID() string {
+	return s.id
+}
+
+func (s *subscriber) UserID() string {
+	return s.userID
+}
+
+func (s *subscriber) Send(msg []byte) {
+	select {
+	case s.msgs <- msg:
+	default:
+		go s.closeSlow()
+	}
 }
 
 func NewHub() *Hub {
 	return &Hub{
+		id:            uuid.NewString(),
 		messageBuffer: 16,
 		writeTimeout:  time.Second * 5,
 		limiter:       rate.NewLimiter(rate.Every(time.Millisecond*100), 8),
 		errorFunc: func(err error) {
-			log.Panic(err)
+			panic(err)
 		},
-		topicFunc: func(r *http.Request) []string {
-			return nil
+		userFunc: func(r *http.Request) (string, []string) {
+			return "", nil
 		},
 	}
 }
 
 func (h *Hub) HandleWebsocket(w http.ResponseWriter, r *http.Request) {
-	err := h.handleWebsocket(w, r, h.topicFunc(r))
+	err := h.handleWebsocket(w, r)
 	if errors.Is(err, context.Canceled) ||
 		websocket.CloseStatus(err) == websocket.StatusNormalClosure ||
 		websocket.CloseStatus(err) == websocket.StatusGoingAway {
@@ -60,7 +89,9 @@ func (h *Hub) HandleWebsocket(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (h *Hub) handleWebsocket(w http.ResponseWriter, r *http.Request, topics []string) error {
+func (h *Hub) handleWebsocket(w http.ResponseWriter, r *http.Request) error {
+	userID, topics := h.userFunc(r)
+
 	var mu sync.Mutex
 	var conn *websocket.Conn
 	var closed bool
@@ -68,6 +99,8 @@ func (h *Hub) handleWebsocket(w http.ResponseWriter, r *http.Request, topics []s
 	ctx := r.Context()
 
 	s := &subscriber{
+		id:     uuid.NewString(),
+		userID: userID,
 		topics: topics,
 		msgs:   make(chan []byte, h.messageBuffer),
 		closeSlow: func() {
@@ -111,28 +144,95 @@ func (h *Hub) handleWebsocket(w http.ResponseWriter, r *http.Request, topics []s
 	}
 }
 
+func (h *Hub) addSubscription(topic string, s Subscription) func() {
+	if subs, ok := h.topics[topic]; ok {
+		subs.subscribers[s.ID()] = s
+		n := subs.users[s.UserID()]
+		subs.users[s.UserID()] = n + 1
+
+		if subs.onNewUser != nil && n == 0 {
+			return func() {
+				h.subscribersMu.RLock()
+				defer h.subscribersMu.RUnlock()
+				for _, sub := range subs.subscribers {
+					subs.onNewUser(s.UserID(), sub)
+				}
+			}
+		}
+		return nil
+	}
+
+	subs := subscriptions{
+		subscribers: map[string]Subscription{s.ID(): s},
+		users:       map[string]int{s.UserID(): 1},
+	}
+	h.topics[topic] = subs
+
+	return func() {
+		h.subscribersMu.RLock()
+		subs.onNewUser(s.UserID(), s)
+		h.subscribersMu.RUnlock()
+
+	}
+}
+
 func (h *Hub) addSubscriber(s *subscriber) {
+	var thunks []func()
+
 	h.subscribersMu.Lock()
 	h.subscribers[s] = struct{}{}
-	for _, t := range s.topics {
-		if subs, ok := h.topics[t]; ok {
-			subs.subscribers[s] = struct{}{}
-		} else {
-			h.topics[t] = subscriptions{subscribers: map[*subscriber]struct{}{s: {}}}
+
+	for _, topic := range s.topics {
+		if thunk := h.addSubscription(topic, s); thunk != nil {
+			thunks = append(thunks, thunk)
 		}
 	}
+
 	h.subscribersMu.Unlock()
+
+	for _, thunk := range thunks {
+		thunk()
+	}
 }
 
 func (h *Hub) deleteSubscriber(s *subscriber) {
+	var thunks []func()
+
 	h.subscribersMu.Lock()
+
 	delete(h.subscribers, s)
-	for _, t := range s.topics {
-		if subs, ok := h.topics[t]; ok {
-			delete(subs.subscribers, s)
+
+	for _, topic := range s.topics {
+		if subs, ok := h.topics[topic]; ok {
+			delete(subs.subscribers, s.id)
+			n := subs.users[s.userID] - 1
+			if n == 0 {
+				delete(subs.users, s.userID)
+			} else {
+				subs.users[s.userID] = n
+			}
+
+			if len(subs.subscribers) == 0 {
+				delete(h.topics, topic)
+			}
+
+			if subs.onRemoveUser != nil && n == 0 {
+				thunks = append(thunks, func() {
+					h.subscribersMu.RLock()
+					defer h.subscribersMu.RUnlock()
+					for _, sub := range subs.subscribers {
+						subs.onRemoveUser(s.UserID(), sub)
+					}
+				})
+			}
 		}
 	}
+
 	h.subscribersMu.Unlock()
+
+	for _, thunk := range thunks {
+		thunk()
+	}
 }
 
 func writeWithTimeout(ctx context.Context, c *websocket.Conn, timeout time.Duration, msg []byte) error {
@@ -149,11 +249,7 @@ func (h *Hub) Send(msg []byte) {
 	defer h.subscribersMu.RUnlock()
 
 	for s := range h.subscribers {
-		select {
-		case s.msgs <- msg:
-		default:
-			go s.closeSlow()
-		}
+		s.Send(msg)
 	}
 }
 
@@ -162,12 +258,8 @@ func (h *Hub) SendTo(topic string, msg []byte) {
 	defer h.subscribersMu.RUnlock()
 
 	if sub, ok := h.topics[topic]; ok {
-		for s := range sub.subscribers {
-			select {
-			case s.msgs <- msg:
-			default:
-				go s.closeSlow()
-			}
+		for _, s := range sub.subscribers {
+			s.Send(msg)
 		}
 	}
 }
